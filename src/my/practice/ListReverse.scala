35e0c05e5f28fe0c883e932fa5fe76a39d6ec70f
umeshau <umeshau125@gmail.com>
2019-09-30 23:30:14 +0530

    Code added

--- src/my/practice/ListReverse.scala
+++ src/my/practice/ListReverse.scala
@@ -1,9 +1,19 @@
 package my.practice
 
+import scala.collection.mutable.ListBuffer
+
 object ListReverse {
   def main(args: Array[String]): Unit = {
 
-    println("hello world")
+    val l = List(1,3,4,7,2,10)
+    println(l.reverse)
 
+    var li : List[Int] = List()
+
+    for(i <- 0 until l.size){
+      li = l(i) :: li
+    }
+
+    println(li)
   }
 }
